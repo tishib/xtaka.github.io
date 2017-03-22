9f0b579e8fb82a70673c4ccabd73ac868c4aaeb8
taka <itakaki0@gmail.com>
2017-03-23 00:15:15 +0900

    Updated a noc

---
+++ sw/network-or-cache/index.js
@@ -0,0 +1,24 @@
+navigator.serviceWorker.register('sw.js', {scope: './controlled'});
+
+navigator.serviceWorker.ready.then(reload);
+
+var sampleIframe = document.getElementById('sample');
+
+sampleIframe.onload = fixHeight;
+
+var reloadBtn = document.querySelector('#reload');
+reloadBtn.onclick = reload;
+
+function reload() {
+  sampleIframe.contentWindow.location.reload();
+}
+
+function fixHeight(evt) {
+  var iframe = evt.target;
+  var document = iframe.contentWindow.document.documentElement;
+  iframe.style.height = document.getClientRects()[0].height + 'px';
+
+  if (window.parent !== window) {
+    window.parent.document.body.dispatchEvent(new CustomEvent('iframeresize'));
+  }
+}
