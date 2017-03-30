e12719b747548bca62a401b5042390a8350a2768
i.takaki <itakaki0@gmail.com>
2017-03-30 15:25:48 +0900

    Create index.js

---
+++ sw/cache-and-update/index.js
@@ -0,0 +1,24 @@
+navigator.serviceWorker.register('/sw/cache-only/sw.js');
+
+navigator.serviceWorker.ready.then(reload);
+
+var sampleIframe = document.getElementById('sample');
+
+sampleIframe.onload = fixHeight;
+
+var reloadBtn = document.querySelector('#reload');
+reloadBtn.onclick = reload;
+
+function fixHeight(evt) {
+  var iframe = evt.target;
+  var document = iframe.contentWindow.document.documentElement;
+  iframe.style.height = document.getClientRects()[0].height + 'px';
+  
+  if (window.parent !== window) {
+    window.parent.document.body.dispatchEvent(new CustomEvent('iframeresize'));
+  }
+}
+
+function reload() {
+  sampleIframe.contentWindow.location.reload();
+}
