c463c6671c8be167c4419912d9f5be2fa6528fa5
i.takaki <itakaki0@gmail.com>
2017-03-30 11:03:24 +0900

    Update sw.js

--- sw/cache-only/sw.js
+++ sw/cache-only/sw.js
@@ -1,3 +1,17 @@
 const CACHE = 'cache-only';
 
-self.addEventListener('install', );
+self.addEventListener('install', (evt) => {
+ console.log('Service worker of this page is being installed.');
+ evt.waitUntil(precache());
+});
+
+self.addEventListener('fetch', (evt) => {
+  console.log('Service worker of this page is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+});
+
+function precache() {
+  return caches.open()
+}
+
+function fromCache() {}
