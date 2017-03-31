d56c8a1ef2f51948c1473ba366cd9dfb51e157af
i.takaki <itakaki0@gmail.com>
2017-03-31 16:38:43 +0900

    Update sw.js

--- sw/cache-and-update/sw.js
+++ sw/cache-and-update/sw.js
@@ -5,4 +5,35 @@ self.addEventListener('install', (evt) => {
   evt.waitUntil(precache());
 });
 
-function precache() {}
+self.addEventListener('fetch', (evt) => {
+  console.log('SW is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+  evt.waitUntill(update(evt.request));
+});
+
+function precache() {
+  return caches.open(CACHE).then((cache) => {
+    return cache.addAll([
+      '/sw/cache-only/index.html',
+      '/sw/cache-only/index.js',
+      '/sw/cache-only/controlled/controlled.html',
+      '/sw/cache-only/controlled/sample.PNG'
+    ]);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE).then((cache) => {
+    return cache.match(request).then((matching) => {
+      return matching || Promise.reject('no match');
+    });
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE).then((cache) => {
+    return fetch(request).then((response) => {
+      return cache.put(request, response);
+    });
+  });
+}
