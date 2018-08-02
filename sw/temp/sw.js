c4e811e8100550c58c4d66d94583b3ee4654b5fa
takaki-ishibashi <itakaki0@gmail.com>
2018-08-03 00:20:53 +0900

    Update basic sw

--- sw/temp/sw.js
+++ sw/temp/sw.js
@@ -4,13 +4,51 @@ const CACHE_NAME = 'cache-v1';
 const CACHE_URLS = [
   'index.html',
 ];
+const RUNTIME = 'runtime';
 
-self.addEventListener('install', () => {
+self.addEventListener('install', event => {
   console.log('on install');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(CACHE_URLS))
+    .then(self.skipWaiting())
+  );
 });
-self.addEventListener('activate', () => {
+self.addEventListener('activate', event => {
   console.log('on activate');
-})
-self.addEventListener('fetch', () => {
+  const currentCaches = [CACHE_URLS, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    })
+    .then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    })
+    .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
   console.log('on fetch');
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open(RUNTIME)
+        .then(cache => {
+          return fetch(event.request)
+          .then(response => {
+            return cache.put(event.request, response.clone())
+            .then(() => {
+              return response;
+            })
+          })
+        })
+      })
+    );
+  }
 });
