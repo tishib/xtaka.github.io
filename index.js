732500815502a14e7e51728fa4151a71829a7628
takaki-ishibashi <itakaki0@gmail.com>
2018-08-02 14:39:42 +0900

    Test with first service worker for me

---
+++ index.js
@@ -0,0 +1,137 @@
+var dataUrl = 'https://api.github.com/events';
+var cacheDelayInput = document.getElementById('cacheDelay');
+var cacheFailInput = document.getElementById('cacheFail');
+var networkDelayInput = document.getElementById('networkDelay');
+var networkFailInput = document.getElementById('networkFail');
+var cacheStatus = document.getElementById('cacheStatus');
+var networkStatus = document.getElementById('networkStatus');
+var getDataButton = document.getElementById('getDataButton');
+var dataOutput = document.getElementById('data');
+
+var cacheName = 'cache-then-network'
+
+// ネットワークの方が早ければそのデータを使う
+var isGotNetworkData = false;
+
+// 取得経過時間 
+var networkGetStartTime;
+var cacheGetStartTiem;
+
+// Reset UI text value and some flags
+function reset() {
+  dataOutput.textContent = '';
+  cacheStatus.textContent = '';
+  networkStatus.textContent = '';
+  isGotNetworkData = false;
+}
+
+function disabelUi() {
+  getDataButton.disabled = true;
+  cacheDelayInput.disabled = true;
+  cacheFailInput.disabled = true;
+  networkDelayInput.disabled = true;
+  networkFailInput.disabled = true;
+  reset();
+}
+
+function updatePage(data) {
+  dataOutput.textContent = 'User ' + data[0].actor.login +
+  ' modified repo ' + data[0].repo.name;
+}
+
+function handleFetchCompletion(response) {
+  var shouldNetworkError = networkFailInput.checked;
+  if (shouldNetworkError) {
+    throw new Error('Network error');
+  }
+
+  var resClone = response.clone();
+  caches.open(cacheName).then(function(cache) {
+    cache.put(dataUrl, resClone);
+  });
+
+  response.json().then(function(data) {
+    updatePage(data);
+    isGotNetworkData = true;
+  });
+}
+
+function handleCacheFetchCompletion(response) {
+  var shouldCacheError = cacheFailInput.checked;
+  if (shouldCacheError || !response) {
+    throw new Error('Cache miss');
+  }
+
+  response.json().then(function(data) {
+    if (!isGotNetworkData) {
+      updatePage(data);
+    }
+  });
+}
+
+function enableUi() {
+  getDataButton.disabled = false;
+  cacheDelayInput.disabled = false;
+  cacheFailInput.disabled = false;
+  networkDelayInput.disabled = false;
+  networkFailInput.disabled = false;
+}
+
+getDataButton.addEventListener('click', function handleClick() {
+  disabelUi();
+
+  networkStatus.textContent = 'Getting...';
+  networkGetStartTime = Date.now();
+
+  var cacheBuster = Date.now();
+  var networkFetch = fetch(dataUrl + '?cacheBuster=' + cacheBuster, {
+      mode: 'cors',
+      cache: 'no-cache',
+    })
+    .then(function(response) {
+      var networkDelay = networkDelayInput.value || 0;
+
+      return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+          try {
+            handleFetchCompletion(response);
+            resolve();
+          } catch (err) {
+            reject(err);
+          }
+        }, networkDelay);
+      });
+    })
+    .then(function() {
+      var now = Date.now();
+      var elapsed = now - networkGetStartTime;
+      networkStatus.textContent = 'Success after ' + elapsed + 'ms';
+    })
+    .catch(function(err) {
+      var now = Date.now();
+      var elapsed = now - networkGetStartTime;
+      networkStatus.textContent = err + ' after ' + elapsed + 'ms';
+    });
+
+    // Get from cached data
+    cacheStatus.textContent = 'Getting...';
+    cacheGetStartTiem = Date.now();
+    var cacheFetch = caches.open(cacheName).then(function(cache) {
+      return cache.match(dataUrl).then(function(response) {
+        var cacheDelay = cacheDelayInput.value || 0;
+
+        return new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            try {
+              handleCacheFetchCompletion(response);
+              resolve();
+            } catch (err) {
+              reject(err);
+            }
+          }, cacheDelay);
+        });
+      });
+    });
+
+    Promise.all([networkFetch, cacheFetch]).then(enableUi);
+});
