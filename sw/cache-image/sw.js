3499982eccfc931dd9a4c9468459cc13efbfdf17
takaki-ishibashi <itakaki0@gmail.com>
2018-08-03 22:38:36 +0900

    update sw.js

--- sw/cache-image/sw.js
+++ sw/cache-image/sw.js
@@ -2,6 +2,7 @@ const CACHE_NAME = 'cache-img-v1';
 const CACHE_URLS = [
   'index.html',
 ];
+const RUNTIME = 'runtime';
 self.addEventListener('install', event => {
   console.log('installing...');
   event.waitUntil(
@@ -15,5 +16,19 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', event => {
   console.log('intercept for fetch request');
-  console.log(event)
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) return cachedResponse;
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            })
+          })
+        })
+      })
+    );
+  }
 });
