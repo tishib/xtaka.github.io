9546d3f2d6fa7a7d560c2fb3549be2a65047cab3
xtaka <itakaki0@gmail.com>
2020-04-14 00:17:31 +0900

    Cleanup odm6us3u proj

--- odm6us3u/server.js
+++ odm6us3u/server.js
@@ -3,12 +3,18 @@ const path = require('path');
 const server = express();
 const compression = require('compression');
 const port = 8000;
+const opts = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['html', 'css']
+  };
+const publishDir = path.join(__dirname, 'public');
 
 server.use(compression());
-server.use(express.static(__dirname));
+server.use(express.static(publishDir, opts));
 
 server.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(publish, 'index.html'));
 });
 
 server.listen(port, () => {
