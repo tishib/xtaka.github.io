71fec89e07e96b696f71891c0c278d8fa3ff144b
takaki_ishibashi <takaki.ishibashi@accenture.com>
2021-10-22 09:37:03 +0900

    wip

--- app/IGWkYCxmpbbb/scripts/index.js
+++ app/IGWkYCxmpbbb/scripts/index.js
@@ -20,7 +20,7 @@ const SVG_AFTER = {
   fillColor: "#008000",
   fillOpacity: 1,
   strokeWeight: 1,
-  strokeColor: "#6c757d",
+  strokeColor: "#006600",
   rotation: 0,
   scale: 5,
   labelOrigin: null,
@@ -29,7 +29,8 @@ const SVG_BEFORE = {
     path: "M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z",
     fillColor: "#008000",
     fillOpacity: 1,
-    strokeWeight: 0,
+    strokeWeight: 1,
+    strokeColor: "#006600",
     rotation: 0,
     scale: 3.5,
     labelOrigin: null,
@@ -211,7 +212,7 @@ function getRTofCurrent(busData) {
 
 const lut = new Map(); // todo
 function calRemainingTime(ttm, busData) {
-  const adjust = 0; // for "time lag" between DB update and real world
+  const adjust = 1; // for "time lag" between DB update and real world
   return (getRTofTimetable(ttm, busData) - getRTofCurrent(busData) - adjust);
 }
 
@@ -369,7 +370,7 @@ function initLocBtn(locationButton, map) {
             lng: position.coords.longitude,
           };
 
-          //pos.lat = TOKYO_STATION_MARUNOUCHI['lat']; pos.lng = TOKYO_STATION_MARUNOUCHI['lng']; // [temp] tokyo station.
+          // pos.lat = TOKYO_STATION_MARUNOUCHI['lat']; pos.lng = TOKYO_STATION_MARUNOUCHI['lng']; // [temp] tokyo station.
           map.setCenter(pos);
           map.setZoom(17);
         
